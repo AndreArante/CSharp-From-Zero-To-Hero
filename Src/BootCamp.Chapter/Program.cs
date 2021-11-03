a0cf81d5515c4bfc99ebcf6262fdebe1e02b3a22
Andre Arante <andrearante12@gmail.com>
2021-11-03 11:00:30 -0700

    Finished Chapter 1 Hw

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,28 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            /* Part One */
+
+            Console.WriteLine("Name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Weight(kg): ");
+            double kg = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Height(cm): ");
+            int cm = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{name} is {age} years old, his weight is {kg} kg and his height is {cm} cm.");
+
+            /* Part Two */
+
+            double BMI = kg / (cm / 100) * (cm / 100);
+            Console.WriteLine($"{name} has a BMI of {BMI}.");
+
         }
     }
 }
